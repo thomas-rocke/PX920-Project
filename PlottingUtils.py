fd7ddff5c9cebfdedcf365d20af607ad64a34de2
Thomas Rocke <45517493+thomas-rocke@users.noreply.github.com>
2022-04-11 16:07:14 +0100

    Sensitivity analysis

--- PlottingUtils.py
+++ PlottingUtils.py
@@ -4,7 +4,11 @@ from MicroMesh import MicroMesh, MicroSolver
 from tqdm import tqdm
 
 
-def Converge(mat_1_params=[10E9, 0.32], mat_2_params=[80E9, 0.22], rel_conc=0.55, nxs=np.arange(3, 50), num_candidates=20):
+def int_logscale(max_n):
+    init_scale = np.logspace(np.log10(3), np.log10(max_n), num=30)
+    return np.unique(init_scale.astype(int))
+
+def Converge(mat_1_params=[10E9, 0.32], mat_2_params=[80E9, 0.22], rel_conc=0.55, nxs=np.arange(3, 8), num_candidates=10):
     E_1, nu_1 = mat_1_params
     E_2, nu_2 = mat_2_params
 
@@ -21,7 +25,7 @@ def Converge(mat_1_params=[10E9, 0.32], mat_2_params=[80E9, 0.22], rel_conc=0.55
     nus = np.zeros_like((Es))
     nu_errs = np.zeros_like((nus))
     nu_tmp = np.zeros_like(E_tmp)
-
+    print(f"Using nx values of {nxs}")
     for i in tqdm(range(len(nxs)), desc="Running through nx values"):
         nx = nxs[i]
         for j in tqdm(range(num_candidates), desc="Testing ensemble of Meshes", leave=False):
@@ -67,7 +71,6 @@ def plot_c_converge(Cs, C_errs, Es, E_errs, nus, nu_errs, real_concs, nnodes, ma
     Es = Es.copy()/1E9
     E_errs = E_errs/1E9
 
-    plt.tight_layout() 
     fig, ax = plt.subplots(3, 3, figsize=(14.0, 14.0))
     
     
@@ -82,11 +85,14 @@ def plot_c_converge(Cs, C_errs, Es, E_errs, nus, nu_errs, real_concs, nnodes, ma
             #ax[i, j].hlines(C_1[i, j], 0, np.max(nnodes), label='Material 1', color='C3', linestyle='dashed')
             #ax[i, j].hlines(C_2[i, j], 0, np.max(nnodes), label='Material 2', color='C4', linestyle='dashed')
             ax[i, j].set_title(f"Convergence of $C_{{{i + 1},{j + 1}}}$")
-            ax[i, j].set_xlabel("Total Number of Nodes", labelpad=-10)
+            ax[i, j].set_xlabel("Total Number of Nodes")
             ax[i, j].set_ylabel("Estimated Elastic Property/GPa")
             ax[i, j].set_xscale('log')
-            ax[i, j].legend()
 
+            # if (i<2 and j<2) or (i==3 and j==3):
+            #     ax[i, j].set_yscale("log")
+            ax[i, j].legend()
+    plt.tight_layout()      
     plt.savefig("C_convergence.png")
 
     E_voigt, nu_voigt = solver.infer_props(voigt_C*1E9)
@@ -126,10 +132,54 @@ def plot_c_converge(Cs, C_errs, Es, E_errs, nus, nu_errs, real_concs, nnodes, ma
     ax[2].set_xscale('log')
     ax[2].legend()
 
+    plt.tight_layout()      
     plt.savefig("E_convergence.png")
 
+def sens_analysis(steps=np.array([10E7, 80E7, 0.0032, 0.0022, 0.006]), x_opt=np.array([10E9, 80E9, 0.32, 0.22, 0.55])):
+    def central_difference(Q_1, Q_2, step):
+        return (Q_2 - Q_1)/(2*step)
 
-props = Converge()
-plot_c_converge(*props)
+    Q_grads = np.zeros((steps.shape[0], 3, 3))
+
+    for i in tqdm(range(steps.shape[0]), desc="Generating Sens for parameters"):
+        x = x_opt
+        x[i] -= steps[i]
+
+        mesh = mesh(*x)
+        solver = solver(mesh)
+        Q_1 = solver.homogenize()
+
+        x = x_opt
+        x[i] += steps[i]
+
+        mesh = mesh(*x)
+        solver = solver(mesh)
+        Q_2 = solver.homogenize()
+
+        Q_grads[i, :, :] = central_difference(Q_1, Q_2, steps[i]) * x_opt # Scaled Sensitivities
+    return Q_grads
+
+
+def plot_sens(Q_grads):
+    fig, ax = plt.subplots(3, 3)
+
+    labels=[r"E$_1$", r"E$_2$", r"$\nu_1$", r"$\nu_2$", r"Volume Fraction"]
+
+    for i in range(3):
+        for j in range(3):
+            ax[i, j].bar(range(Q_grads.shape[0]), Q_grads[:, i, j])
+            ax[i, j].set_xticks(range(Q_grads.shape[0], labels))
+            ax[i, j].set_xlabel("Parameter")
+            ax[i, j].set_ylabel("Scaled Sensitivity")
+            ax[i, j].set_title(f"Sensitivity of $C_{{{i + 1},{j + 1}}}$")
+    plt.tight_layout()
+
+    plt.savefig("Sensitivities.png")
+
+
+
+
+# props = Converge()
+# plot_c_converge(*props)
 
     
